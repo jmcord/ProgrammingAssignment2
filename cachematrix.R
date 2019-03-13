8197ca2cb3c25fe5f986b031a7bb434b23be7f33
Chemical <48360043+jmcord@users.noreply.github.com>
2019-03-13 18:32:37 +0100

    Version 2.0
    General comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function makeCacheMatrix creates a Matrix object that will storage the inverse calculated by means of
+## the cacheSolve function. This will prevent from calculating the inverse of the same matrix multiple times
+## saving computational effort
+
 
 ## This function creates a special "matrix" object that can cache its inverse
 
@@ -24,7 +26,9 @@ makeCacheMatrix <- function(x = numeric()) {
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
   inv <- x$getinv()
